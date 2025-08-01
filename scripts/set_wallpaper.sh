37c71a3e99c23b1e1f1794422dfdc26bbbbf480f
azubirif <alejandrozubirifunes@gmail.com>
2025-08-02 00:41:00 +0200

    Ahora mejor con Waybar

--- scripts/set_wallpaper.sh
+++ scripts/set_wallpaper.sh
@@ -13,10 +13,27 @@ swww img ~/current-wallpaper --transition-type center
 wal -i $1 -n > /dev/null 2>&1 &
 
 # Actualizamos Mako
-bash ./update_mako.sh
+. "${HOME}/.cache/wal/colors.sh"
+
+conffile="${HOME}/dotfiles/.config/mako/config"
+
+# Associative array, color name -> color code.
+declare -A colors
+colors=(
+    ["background-color"]="${background}89"
+    ["text-color"]="$foreground"
+    ["border-color"]="$color13"
+)
+
+for color_name in "${!colors[@]}"; do
+  # replace first occurance of each color in config file
+  sed -i "0,/^$color_name.*/{s//$color_name=${colors[$color_name]}/}" $conffile
+done
+
 makoctl reload
 
 # Actualizamos Waybar
-killall -SIGUSR2 waybar
+pkill waybar
+waybar > /dev/null 2>&1 &
 
 notify-send "Wallpaper actualizado" "Nuevo: $1"
