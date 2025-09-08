3b53a229f02f3c682dcfffe3dbf70e54be076792
azubirif <alejandrozubirifunes@gmail.com>
2025-09-08 18:57:01 +0200

    rofi

--- .config/rofi/leave/leave.sh
+++ .config/rofi/leave/leave.sh
@@ -4,7 +4,7 @@ choice=$(printf "Lock\nLogout\nSuspend\nReboot\nShutdown" | rofi -dmenu)
 if [[ $choice == "Lock" ]];then
     bash ~/.config/system_scripts/wayland_session_lock
 elif [[ $choice == "Logout" ]];then
-    pkill -KILL -u "$USER"
+    log_out
 elif [[ $choice == "Suspend" ]];then
     systemctl suspend
 elif [[ $choice == "Reboot" ]];then
